fab4f3c32a35353bd41a63463d7cb3d11cb6f9f8
Narayani-19 <narayani.1904@gmail.com>
2025-09-09 23:28:20 +0530

    Created

---
+++ Java/Assignment4/Q2.java
@@ -0,0 +1,10 @@
+/* Write a program to create a class named 'Student' with members 'name', 'roll', ‘branch’. Declare two methods to input the student details and display the details of the student.
+Create a Main class to test the functionalities of the above class.*/
+import java.util.scanner;
+class Student{
+    String name,branch;
+    int roll;
+    void getter()
+  {
+      System.out.println("Enter namme,branch and roll_no respectively');
+      name=
