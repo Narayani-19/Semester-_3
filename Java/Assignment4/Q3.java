c56f678a8d470b6ec04bc171c940a437a639da6b
Narayani-19 <narayani.1904@gmail.com>
2025-09-09 23:45:14 +0530

    Created

---
+++ Java/Assignment4/Q3.java
@@ -0,0 +1,2 @@
+/*  Declare a class Complex with member real and imaginary part. Define a method initialise() to input the two complex numbers, show() to display the 
+complex number and add() to add the two complex numbers.Declare another class to illustrate the operations of the Complex class.*/
