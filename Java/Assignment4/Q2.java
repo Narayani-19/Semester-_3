0c6861e764bdb05806be0c3ddec9e1e940b81d9d
Narayani-19 <narayani.1904@gmail.com>
2025-09-09 23:40:14 +0530

    Updated

--- Java/Assignment4/Q2.java
+++ Java/Assignment4/Q2.java
@@ -4,7 +4,24 @@ import java.util.scanner;
 class Student{
     String name,branch;
     int roll;
-    void getter()
-  {
-      System.out.println("Enter namme,branch and roll_no respectively');
-      name=
+    public static void getter(){
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter name, branch and roll_no respectively");
+      name = sc.next();
+      branch = sc.next();
+      roll = sc.nextInt();
+    }
+    public static void display()
+    {
+        System.out.println("Name:-"+name);
+        System.out.println("Name:-"+branch);
+        System.out.println("Name:-"+roll);
+    }
+}    
+class CallStudent{
+    public static void main(String s[])
+    {
+        Student.getter();
+        Student.display();
+    }
+}
