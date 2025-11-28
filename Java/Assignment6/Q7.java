02fdbd9b89df4e6f41deee94dec58d0c448ea619
Narayani Kundu <narayani.1904@gmail.com>
2025-11-28 22:58:21 +0530

    uploaded

---
+++ Java/Assignment6/Q7.java
@@ -0,0 +1,14 @@
+import btech.arithmetic.MyMath;
+
+class Test2 {
+    public static void main(String[] args) {
+
+        MyMath m = new MyMath();
+
+        System.out.println(m.add(10,5));
+        System.out.println(m.sub(10,5));
+        System.out.println(m.mul(10,5));
+        System.out.println(m.div(10,5));
+        System.out.println(m.mod(10,5));
+    }
+}
