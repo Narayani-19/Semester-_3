0ae701317aae4ea10b75ef2ddf777ef890b19f02
Narayani Kundu <narayani.1904@gmail.com>
2025-11-28 22:59:27 +0530

    uploaded

---
+++ Java/Assignment6/org/shapes/Square.java
@@ -0,0 +1,6 @@
+package org.shapes;
+
+public class Square {
+    public void area(int s) { System.out.println("Square Area: " + (s*s)); }
+    public void peri(int s) { System.out.println("Square Perimeter: " + (4*s)); }
+}
