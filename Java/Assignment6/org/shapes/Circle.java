cd109e411918c75246060b83c7864ce6758f32b7
Narayani Kundu <narayani.1904@gmail.com>
2025-11-28 22:59:49 +0530

    uploaded

---
+++ Java/Assignment6/org/shapes/Circle.java
@@ -0,0 +1,6 @@
+package org.shapes;
+
+public class Circle {
+    public void area(int r) { System.out.println("Circle Area: " + (3.14*r*r)); }
+    public void peri(int r) { System.out.println("Circle Perimeter: " + (2*3.14*r)); }
+}
