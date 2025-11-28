a6bcea4f7302d35d2d308d6ffe52db1877c197c2
Narayani Kundu <narayani.1904@gmail.com>
2025-11-28 23:00:12 +0530

    uploaded

---
+++ Java/Assignment6/org/shapes/Triangle.java
@@ -0,0 +1,6 @@
+package org.shapes;
+
+public class Triangle {
+    public void area(int b, int h) { System.out.println("Triangle Area: " + (0.5*b*h)); }
+    public void peri(int a, int b, int c) { System.out.println("Triangle Perimeter: " + (a+b+c)); }
+}
