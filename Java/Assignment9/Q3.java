88fe36f34a32fc73b04e67e8d13b32cd6a9d1144
Narayani Kundu <narayani.1904@gmail.com>
2025-11-28 23:28:05 +0530

    uploaded

---
+++ Java/Assignment9/Q3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String s = sc.nextLine();
+
+        StringBuffer sb = new StringBuffer(s.toUpperCase());
+        System.out.println(sb);
+    }
+}
