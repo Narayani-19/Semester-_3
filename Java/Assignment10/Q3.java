c5b1277e8b8bc778abc8b912f0952608e6e74edd
Narayani Kundu <narayani.1904@gmail.com>
2025-11-28 23:39:11 +0530

    uploaded

--- Java/Assignment10/Q3.java
+++ Java/Assignment10/Q3.java
@@ -1,47 +1,86 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.*;
 
-public class Assignment3 extends Frame implements ActionListener {
-    TextField t;
-    Button b1, b2;
+public class Assignment3 extends Frame {
 
-    Assignment3() {
-        setSize(400, 300);
-        setLayout(null);
+    private TextField textField1, textField2, resultField;
+    private Button subtractButton,addButton;
 
-        t = new TextField("Welcome");
-        t.setBounds(100, 100, 120, 30);
-
-        b1 = new Button("Show");
-        b1.setBounds(80, 200, 70, 30);
-
-        b2 = new Button("Hide");
-        b2.setBounds(180, 200, 70, 30);
-
-        add(t);
-        add(b1);
-        add(b2);
-
-        b1.addActionListener(this);
-        b2.addActionListener(this);
-
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
+    public Assignment3() { //constructor
+        // Set up the frame
+        setTitle("Assignment-3");
+        setSize(300, 250);
+         this.addWindowListener(new WindowAdapter() {
+            // Anonymous class to override windowClosing event
+            public void windowClosing(WindowEvent e)
+            {
+                // Call dispose method
                 System.exit(0);
             }
         });
+        setLayout(null);
 
-        setVisible(true);
+        // Create text fields and button
+        textField1 = new TextField();
+        textField2 = new TextField();
+        resultField = new TextField();
+        resultField.setEditable(false); // Result field should not be editable
+        subtractButton = new Button("-");
+        addButton = new Button("+");
+
+        // Set bounds for the components
+         textField1.setBounds(30, 50, 130, 30);
+         textField2.setBounds(30, 100, 130, 30);
+         resultField.setBounds(30, 150, 130, 30);
+         subtractButton.setBounds(150, 200, 80, 40);
+ addButton.setBounds(50, 200, 80, 40);
+
+        // Add components to the frame
+         add(textField1);
+         add(textField2);
+         add(resultField);
+
+         add(subtractButton);
+ add(addButton);
+
+         // Add action listener to the subtract button  
+         subtractButton.addActionListener(new ActionListener() {
+             public void actionPerformed(ActionEvent e) {
+                 try {
+                      int num1 = Integer.parseInt(textField1.getText());
+                      int num2 = Integer.parseInt(textField2.getText());
+                      int result = num1 - num2;
+                      resultField.setText(Integer.toString(result));
+                 } catch (NumberFormatException ex) {
+                    resultField.setText("Invalid Input");
+                 }
+             }
+         });
+ // Add action listener to the add button  
+
+         addButton.addActionListener(new ActionListener() {
+             public void actionPerformed(ActionEvent e) {
+                 try {
+                      int num1 = Integer.parseInt(textField1.getText());
+                      int num2 = Integer.parseInt(textField2.getText());
+                      int result = num1 + num2;
+                      resultField.setText(Integer.toString(result));
+                 } catch (NumberFormatException ex) {
+                    resultField.setText("Invalid Input");
+                 }
+             }
+         });
+
+
+
     }
 
-    public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==b1)
-            t.setVisible(true);
-        if(e.getSource()==b2)
-            t.setVisible(false);
-    }
+    public static void main(String[] args) {
+        Assignment3 frame = new Assignment3();
 
-    public static void main(String args[]) {
-        new Assignment3();
+        frame.setVisible(true);
     }
 }
+
